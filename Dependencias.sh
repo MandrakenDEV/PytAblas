4da640880b1ba54b570835c4d543b1081fabe50a
MandrakenDEV <93348089+MandrakenDEV@users.noreply.github.com>
2022-05-28 15:39:21 -0600

    Add files via upload

---
+++ Dependencias.sh
@@ -0,0 +1,1 @@
+apt-get install python3 && apt-get update && apt-get upgrade
---
+++ PytAblas.py
@@ -0,0 +1,6 @@
+print("Ingrese número  para ver la tabla...")
+n = int(input())
+for i in range (0, 13):
+   print(n," x ",i, " = ",n*i)
+print("Creado por MandrakenDEV en 2022")
+
